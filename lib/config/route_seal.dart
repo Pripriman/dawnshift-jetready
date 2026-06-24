1ea5fdeee510aaf07dbace9fdf85fecdc7f77dce
Mara Lindqvist <mara.lindqvist@users.noreply.github.com>
2026-06-24 14:37:49 +0300

    ci: wire content blob + fix codemagic.yaml (drop iOS publishing, harden dart_defines)

--- lib/config/route_seal.dart
+++ lib/config/route_seal.dart
@@ -2,9 +2,9 @@ import 'dart:io' show Platform;
 
 class RouteSeal {
   static const String _android =
-      'UnlwtLJDdl1H6pSUxUO-yHlEXgV_1bRdrDIIeGoh7yFd945fYcCaTS9I4bbroDmE1vhBlC-eHuXRYA';
+      '5qcBMBmzSfcfnRmYc+vaZNNwg+1Ij7hp0fWXgKoKPBE/BdAETbZOzXoReuhlBecHXmEaW/uJATOZ2es=';
   static const String _ios =
-      '6sFt8EIbG5yPc29OnAaMUyDq7xQPUwtP9dMD95ntbyHI4WX-ugnYRPZUUSHqLe3bXrieVwOAH6QC5A';
+      '+UIG1Xx50qCfftl3+xHXldykXssFMbQkg85TNs173sY79mKhSt9UozpF2OAQhuu5hTTdtQPqLL2CB/o=';
 
   static String forPlatform() => Platform.isIOS ? _ios : _android;
 }
